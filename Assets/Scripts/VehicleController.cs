9e18334389b07975e70cd973393453551691a3dd
Faez Mahmoudi <faez.mahmudi.1373@gmail.com>
2026-06-30 12:18:23 -0700

    Vehicle Scene Design p6
    Update  VehicleController.cs to have a different gameplay mechanic(translate to left and right on button press)

--- Assets/Scripts/VehicleController.cs
+++ Assets/Scripts/VehicleController.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 // INHERITANCE
 public class VehicleController : PlayerController
 {
-    //[SerializeField] private Animator carAnim;
     [SerializeField] private VehicleJump vehicleJump;
+    private Vector3 offset = new Vector3(5f, 0f, 0f);
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,9 +15,9 @@ public class VehicleController : PlayerController
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
-
+        //horizontalInput = Input.GetAxis("Horizontal");
+        //forwardInput = Input.GetAxis("Vertical");
+        
         Move();
 
         if(Input.GetKeyDown(KeyCode.C))
@@ -29,6 +29,15 @@ public class VehicleController : PlayerController
         {
             SpecialMove();   
         }  
+    }
+
+    protected override void Move()
+    {
+        if (Input.GetKeyDown(KeyCode.A) && transform.position != -offset)
+            transform.position -= offset;
+        else if (Input.GetKeyDown(KeyCode.D) && transform.position != offset)
+            transform.position += offset;
+
     }
 
     // POLYMORPHISM
--- Assets/Scripts/VehicleJump.cs
+++ Assets/Scripts/VehicleJump.cs
@@ -3,28 +3,18 @@ using UnityEngine;
 public class VehicleJump : MonoBehaviour
 {
     public bool isOnGrand = true;
-    //[SerializeField] private Rigidbody vehicleRb;
-    //[SerializeField] private float jumpForce = 2000;
     private Animator carAnim;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //vehicleRb = GetComponent<Rigidbody>();
         carAnim = GetComponent<Animator>();
-    }
-
-    void Update()
-    {
-        //if (isOnGrand)
-          //  carAnim.SetBool("isSelected", false);
     }
 
     public void Jump()
     {
+        // set jump animation
         carAnim.SetBool("isSelected", true);
-        //if (isOnGrand)
-            //vehicleRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
 
     private void OnCollisionEnter(Collision collision)
