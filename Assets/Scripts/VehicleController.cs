ca65ea766a5225d662bf532435ca6f7baa34de1d
Faez Mahmoudi <faez.mahmudi.1373@gmail.com>
2026-07-01 02:44:39 -0700

    Vehicle Scene Design p9
    Update the VehicleController.cs to rotate the car smoothly between lanes

--- Assets/Scripts/VehicleController.cs
+++ Assets/Scripts/VehicleController.cs
@@ -4,17 +4,13 @@ using UnityEngine;
 public class VehicleController : PlayerController
 {
     [SerializeField] private VehicleJump vehicleJump;
-    private Vector3 offset = new Vector3(5f, 0f, 0f);
-
-    //private Vector3 targetPosition;
     [SerializeField] private float laneDistance = 5f;
+    [SerializeField] private float tiltAngle = 15f;
+    [SerializeField] private float tiltSpeed = 8f;
     private int currentLane = 0;
+    private float targetRotationY = 0f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        //targetPosition = transform.position;
-    }
+    
 
     // Update is called once per frame
     void Update()
@@ -22,24 +18,24 @@ public class VehicleController : PlayerController
         Move();
 
         // move smoothly
-        //transform.position = Vector3.Lerp(transform.position, targetPosition, speed * Time.deltaTime);
         Vector3 pos = transform.position;
-
         float targetX = currentLane * laneDistance;
-
         pos.x = Mathf.MoveTowards(pos.x, targetX, speed * Time.deltaTime);
-
         transform.position = pos;
 
+        // rotate smoothly
+        Quaternion targetRotation = Quaternion.Euler(transform.eulerAngles.x, targetRotationY, transform.eulerAngles.z);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, tiltSpeed * Time.deltaTime);
+
+        // make straigh on y
+        if (Mathf.Abs(pos.x - targetX) < 0.05f)
+            targetRotationY = 0f;
+
         if(Input.GetKeyDown(KeyCode.C))
-        {
             SwitchCamera();
-        }
 
         if(Input.GetKeyDown(KeyCode.Space))
-        {
-            SpecialMove();   
-        }  
+            SpecialMove();    
     }
 
     // POLYMORPHISM  
@@ -47,10 +43,15 @@ public class VehicleController : PlayerController
     {
         // Move the player to the left and right
         if (Input.GetKeyDown(KeyCode.A) && currentLane > -1)
+        {
             currentLane--;
+            targetRotationY = -tiltAngle;
+        }
         else if (Input.GetKeyDown(KeyCode.D) && currentLane < 1)
+        {
             currentLane++;
-
+            targetRotationY = tiltAngle;
+        }
     }
 
     // POLYMORPHISM
