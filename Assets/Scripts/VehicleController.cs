fe09a88eeb28e7538eed1bf22be4152a65a96910
Faez Mahmoudi <faez.mahmudi.1373@gmail.com>
2026-07-01 02:25:21 -0700

    Vehicle Scene Design p8
    Update the VehicleController.cs to move the car smoothly between lanes

--- Assets/Scripts/VehicleController.cs
+++ Assets/Scripts/VehicleController.cs
@@ -6,16 +6,30 @@ public class VehicleController : PlayerController
     [SerializeField] private VehicleJump vehicleJump;
     private Vector3 offset = new Vector3(5f, 0f, 0f);
 
+    //private Vector3 targetPosition;
+    [SerializeField] private float laneDistance = 5f;
+    private int currentLane = 0;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        //targetPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {   
         Move();
+
+        // move smoothly
+        //transform.position = Vector3.Lerp(transform.position, targetPosition, speed * Time.deltaTime);
+        Vector3 pos = transform.position;
+
+        float targetX = currentLane * laneDistance;
+
+        pos.x = Mathf.MoveTowards(pos.x, targetX, speed * Time.deltaTime);
+
+        transform.position = pos;
 
         if(Input.GetKeyDown(KeyCode.C))
         {
@@ -32,10 +46,10 @@ public class VehicleController : PlayerController
     protected override void Move()
     {
         // Move the player to the left and right
-        if (Input.GetKeyDown(KeyCode.A) && transform.position != -offset)
-            transform.position -= offset;
-        else if (Input.GetKeyDown(KeyCode.D) && transform.position != offset)
-            transform.position += offset;
+        if (Input.GetKeyDown(KeyCode.A) && currentLane > -1)
+            currentLane--;
+        else if (Input.GetKeyDown(KeyCode.D) && currentLane < 1)
+            currentLane++;
 
     }
 
