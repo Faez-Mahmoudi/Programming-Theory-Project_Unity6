60303b5861a59985a8b5fb280cb009b14ec5c387
Faez Mahmoudi <faez.mahmudi.1373@gmail.com>
2026-07-02 12:19:13 -0700

    Vehicle Scene Design p10
    Create a new MoveRight.cs script to add to obstacles

---
+++ Assets/Scripts/MoveRight.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class MoveRight : MonoBehaviour
+{
+    [SerializeField] private float speed = 20.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Stop move left on gameOver
+        //if(MainManager.Instance.isGameActive)
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
