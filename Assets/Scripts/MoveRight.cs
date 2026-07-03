af181dceeda931ecfd6941f3215fee661356fb62
Faez Mahmoudi <faez.mahmudi.1373@gmail.com>
2026-07-03 01:56:28 -0700

    Vehicle Scene Design p12
    Update MoveRight.cs

--- Assets/Scripts/MoveRight.cs
+++ Assets/Scripts/MoveRight.cs
@@ -16,5 +16,8 @@ public class MoveRight : MonoBehaviour
         // Stop move left on gameOver
         //if(MainManager.Instance.isGameActive)
             transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        if (gameObject.transform.position.z < -5.0f || gameObject.transform.position.y < 0)
+            Destroy(gameObject);
     }
 }
