f129240149b067d0b401e79e1d194ebe59b39466
Faez Mahmoudi <faez.mahmudi.1373@gmail.com>
2026-07-08 21:03:34 -0700

    Armor Scene Design p3
    Do some design on armor scene and add vehicles to it

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -3,10 +3,22 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    [SerializeField] private Vector3 offset = new Vector3(0, 3, -7);
+    [SerializeField] private Vector3 offset = new Vector3(0, 4.5f, -8);
+    [SerializeField] private float rotationSpeed = 100.0f;
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Rotate(horizontalInput);
+    }
     
     void LateUpdate()
     {
         transform.position = new Vector3(player.transform.position.x, 0, player.transform.position.z)  + offset;
+    }
+
+    public void Rotate(float input)
+    {
+        transform.Rotate(Vector3.up, input * rotationSpeed * Time.deltaTime); 
     }
 }
