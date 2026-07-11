eb1704489340b677b9457404f77ff22c583fc089
Faez Mahmoudi <faez.mahmudi.1373@gmail.com>
2026-07-11 01:58:30 -0700

    Vehicle Scene Design p14
    Do some problem solving on vehicle scene

--- Assets/Scripts/MoveRight.cs
+++ Assets/Scripts/MoveRight.cs
@@ -4,12 +4,6 @@ public class MoveRight : MonoBehaviour
 {
     [SerializeField] private float speed = 20.0f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
@@ -17,7 +11,7 @@ public class MoveRight : MonoBehaviour
         //if(MainManager.Instance.isGameActive)
             transform.Translate(Vector3.forward * Time.deltaTime * speed);
 
-        if (gameObject.transform.position.z < -5.0f || gameObject.transform.position.y < 0)
+        if (gameObject.transform.position.z < -10.0f || gameObject.transform.position.y < 0)
             Destroy(gameObject);
     }
 }
